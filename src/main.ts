c8edce08a7b983022cb4cdf5557a602420158638
Alejandra Veronica Terceros Arcani <alita4774@gmail.com>
2025-09-21 11:41:28 -0400

    feat(pwa): enable service worker and web manifest

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { Home } from './app/home/home';
-
-bootstrapApplication(Home, appConfig)
-  .catch((err) => console.error(err));
-
-/*bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));*/
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
+bootstrapApplication(App, {
+  providers: [
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
+  ],
+});
